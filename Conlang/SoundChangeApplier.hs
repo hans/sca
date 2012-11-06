4eee0a80dc777090912b09c3355e0534bb98d314
Hans Engel <engel@engel.uk.to>
2012-11-06 11:22:19 -0700

    Redo without regexes. Basic rule application working

--- Conlang/SoundChangeApplier.hs
+++ Conlang/SoundChangeApplier.hs
@@ -1,8 +1,6 @@
 module Conlang.SoundChangeApplier where
 
 import Data.String.Utils (replace)
-import Text.Regex (mkRegex, subRegex)
-import Text.Regex.Posix
 
 -- A string of phonemes
 type Phoneme = Char
@@ -11,25 +9,14 @@ type Phoneme = Char
 type PhonemeClass = (Char, [Phoneme])
 
 -- Rule for matching a phoneme or phoneme class within a context
-type Pattern = String
-
 data Rule =
-     Rule { matchPattern :: Pattern,
-            replacement  :: String }
+     Rule { matchChar   :: Char,
+            replacement :: String }
 
--- Replace any occurrences of a phoneme class label with its respective
--- regular expression character class.
-qualifyPatternForClass :: Pattern -> PhonemeClass -> Pattern
-qualifyPatternForClass input (k, phonemes) = let reClass = ")[" ++ phonemes ++ "]("
-                                             in replace [k] reClass input
+applyRule :: String -> Rule -> String
+applyRule (c:cs) r@(Rule l ps) | c == l     = ps ++ applyRule cs r
+                               | otherwise = c : applyRule cs r
+applyRule ""     _                         = ""
 
--- Qualify a user-defined pattern (i.e., replace custom character
--- classes with literals)
-qualifyPattern :: Pattern -> [PhonemeClass] -> Pattern
-qualifyPattern = foldl qualifyPatternForClass
-
-applyChange :: String -> Rule -> String
-applyChange s (Rule p r) = subRegex (mkRegex p) r s
-
-applyChanges :: String -> [Rule] -> String
-applyChanges = foldl applyChange
+applyRules :: String -> [Rule] -> String
+applyRules = foldl applyRule
