eddb16d67005d5ab304238b265103f9855902ae8
Hans Engel <engel@engel.uk.to>
2012-11-07 10:59:58 -0700

    Add phoneme class types (detection not yet functional)

--- Conlang/SoundChangeApplier.hs
+++ Conlang/SoundChangeApplier.hs
@@ -5,23 +5,35 @@ import Data.String.Utils (replace)
 -- A string of phonemes
 type Phoneme = Char
 
+-- A pair of phoneme class and corresponding phonemes
+type PhonemeClass = (Char, [Phoneme])
+
 -- Matching context (combination of phonemes and phoneme classes)
 type Context = String
 
 -- Rule for matching a phoneme or phoneme class within a context
 data Rule =
-     Rule { matchChar     :: Char,
-            replacement   :: [Phoneme],
-            beforeContext :: [Phoneme],
-            afterContext  :: [Phoneme] }
+       PhonemeRule { matchPhoneme  :: Phoneme,
+                     replacement   :: [Phoneme],
+                     beforeContext :: Context,
+                     afterContext :: Context }
+     | PhonemeClasRule { matchChar     :: Char,
+                         replacement   :: [Phoneme],
+                         beforeContext :: [Phoneme],
+                         afterContext  :: [Phoneme] }
 
-applyRule :: String -> Rule -> String
-applyRule (c:cs) r@(Rule l ps _ _) | c == l     = ps ++ applyRule cs r
-                                   | otherwise = c : applyRule cs r
-applyRule ""     _                             = ""
+-- Rule application
 
-applyRules :: String -> [Rule] -> String
-applyRules = foldl applyRule
+applyRule :: [PhonemeClass] -> String -> Rule -> String
+
+applyRule classes (c:cs) r@(PhonemeRule l ps _ _)
+          | c == l     = ps ++ applyRule classes cs r
+          | otherwise  = c : applyRule classes cs r
+
+applyRule _ "" _ = ""
+
+applyRules :: [PhonemeClass] -> String -> [Rule] -> String
+applyRules classes = foldl (applyRule classes)
 
 matchContext :: Context -> String -> Bool
 matchContext = (==)
