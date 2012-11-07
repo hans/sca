664106cc8829ee320f078e8c1960d2e905d57051
Hans Engel <engel@engel.uk.to>
2012-11-07 10:48:19 -0700

    Begin with match contexts

--- Conlang/SoundChangeApplier.hs
+++ Conlang/SoundChangeApplier.hs
@@ -5,15 +5,23 @@ import Data.String.Utils (replace)
 -- A string of phonemes
 type Phoneme = Char
 
+-- Matching context (combination of phonemes and phoneme classes)
+type Context = String
+
 -- Rule for matching a phoneme or phoneme class within a context
 data Rule =
-     Rule { matchChar   :: Char,
-            replacement :: [Phoneme] }
+     Rule { matchChar     :: Char,
+            replacement   :: [Phoneme],
+            beforeContext :: [Phoneme],
+            afterContext  :: [Phoneme] }
 
 applyRule :: String -> Rule -> String
-applyRule (c:cs) r@(Rule l ps) | c == l     = ps ++ applyRule cs r
-                               | otherwise = c : applyRule cs r
-applyRule ""     _                         = ""
+applyRule (c:cs) r@(Rule l ps _ _) | c == l     = ps ++ applyRule cs r
+                                   | otherwise = c : applyRule cs r
+applyRule ""     _                             = ""
 
 applyRules :: String -> [Rule] -> String
 applyRules = foldl applyRule
+
+matchContext :: Context -> String -> Bool
+matchContext = (==)
