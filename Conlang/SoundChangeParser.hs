de36c1f47c5e225621d7dc6a2cca3dc4e19651db
Hans Engel <engel@engel.uk.to>
2012-12-14 21:47:18 -0700

    Rearrange; abstract lower -> anyPhoneme

--- Conlang/SoundChangeParser.hs
+++ Conlang/SoundChangeParser.hs
@@ -20,19 +20,13 @@ data Rule = Rule { replacement   :: [Phoneme],
 instance Show Rule where
     show (Rule r b i a) = show r
 
+file :: Parsec String PhonemeClassMap [Rule]
+file = many phonemeClassDefinition >> many rule
+
 phonemeClassDefinition :: Parsec String PhonemeClassMap ()
 phonemeClassDefinition = modifyState =<< modifier
                          where modifier = liftM2 insert upper defn
                                defn     = char ':' >> spaces >> many1 lower
-
-context :: Parsec String PhonemeClassMap Context
-context = many1 (classContext <|> phonemeContext)
-
-classContext :: Parsec String PhonemeClassMap ContextElement
-classContext = liftM oneOf $ liftM2 (!) getState upper
-
-phonemeContext :: Parsec String PhonemeClassMap ContextElement
-phonemeContext = liftM char lower
 
 rule :: Parsec String PhonemeClassMap Rule
 rule = do
@@ -43,8 +37,17 @@ rule = do
 
     return $ Rule replacement beforeContext inContext afterContext
 
-file :: Parsec String PhonemeClassMap [Rule]
-file = many phonemeClassDefinition >> many rule
+context :: Parsec String PhonemeClassMap Context
+context = many1 (classContext <|> phonemeContext)
+
+classContext :: Parsec String PhonemeClassMap ContextElement
+classContext = liftM oneOf $ liftM2 (!) getState upper
+
+phonemeContext :: Parsec String PhonemeClassMap ContextElement
+phonemeContext = liftM char anyPhoneme
+
+anyPhoneme :: Parsec String PhonemeClassMap Phoneme
+anyPhoneme = lower
 
 main :: IO ()
 main = do
